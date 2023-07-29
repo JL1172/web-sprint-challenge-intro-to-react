111e088c5b8e59d3f9abbcfb81d581b9c048b83d
Jacob Lang <jacoblang72@comcast.net>
2023-07-28 21:54:29 -0400

    wip

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -4,14 +4,71 @@ import { keyframes } from "styled-components";
 import ReactStrap from 'reactstrap';
 import Films from "./Films";
 
+const rebelLogo = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhHsIzV5mNwSz_HiczMCdLiSOcWN7iTmuRQyxuVhx2wgcXvf7T726HmCZ8u7XUHx3XJjQ&usqp=CAU";
+const sithLogo = 'https://www.iconarchive.com/download/i103540/sensibleworld/starwars/Lightsaber-Red.1024.png';
+const kf2 = keyframes`
+20% {
+    border-left-color :  royalblue;
+    border-left-color: royalblue;
+    
+}
+30% {
+    border-left-color : crimson;
+    border-top-color: crimson;
+}
+40% {
+    border-left-color : #22BA1A;
+    border-top-color: #22BA1A;
+}
+50% {
+    border-left-color : royalblue;
+    border-top-color: royalblue;
+   
+}
+60% {
+    border-left-color : crimson;
+    border-top-color: crimson;
+    
+}
+70% {
+    border-left-color : #22BA1A;
+    border-top-color: #22BA1A;
+}
+80% {
+    border-left-color :  royalblue;
+    border-top-color : royalblue;
+}
+90% {
+    border-left-color :  crimson;
+    border-top-color : crimson;
+}
+100% {
+    border-left-color :  #22BA1A;
+    border-top-color : #22BA1A; 
+}
+`
+
 const StyledDiv = styled.div`
+padding: 1rem;
+border-left : 2px solid gray;
+border-top : 2px solid gray;
+border-radius : 5px;
 display: ${props => props.result ? 'flex' : 'none'};
 flex-direction : column;
 align-items : flex-start;
 margin-left: 5rem;
-opacity : 0;
 opacity : ${props => props.result ? 1 : 0};
-transition : .4s;
+backdrop-filter : blur(10px);
+padding-bottom : 5rem;
+margin-bottom : 1rem;
+margin-right : 5rem;
+color : whitesmoke;
+animation : ${kf2} 10s ease-in-out infinite; 
+img {
+    height: 100px;
+    width : 100px;
+    background-color : transparent; 
+}
 `
 
 
@@ -23,13 +80,14 @@ export default function Accordian(props) {
         <>
         <StyledDiv result = {result}>
             <h5>Birth Year: {birth_year}</h5>
-            <h5>Weight : {mass}</h5>
+            <h5>Weight : {mass}Kg</h5>
+            <h5>Movies:</h5>
             { films.map((film,i)=> {
                return <Films key = {i}
                 movie = {film}/>
             })   
             }
-          
+            <img src ={name === 'Darth Vader' ? sithLogo : rebelLogo}/>
         </StyledDiv>
         </>
     )
