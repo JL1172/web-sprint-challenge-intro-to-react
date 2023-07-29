85c962d5569958ecff005b4384ae6403b37b33b8
Jacob Lang <jacoblang72@comcast.net>
2023-07-29 12:16:51 -0400

    revertedbackwithstyledcomp

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -1,91 +1,91 @@
 import React, {useState, useEffect} from "react";
-// import styled from "styled-components";
-// import { keyframes } from "styled-components";
-// import ReactStrap from 'reactstrap';
+import styled from "styled-components";
+import { keyframes } from "styled-components";
+import ReactStrap from 'reactstrap';
 import Films from "./Films";
 
 const rebelLogo = "https://cdn.icon-icons.com/icons2/318/PNG/512/Death-Star-icon_34500.png";
 const sithLogo = 'https://www.iconarchive.com/download/i103540/sensibleworld/starwars/Lightsaber-Red.1024.png';
-// const kf2 = keyframes
-// 20% {
-//     border-left-color :  royalblue;
-//     border-left-color: royalblue;
+const kf2 = keyframes`
+20% {
+    border-left-color :  royalblue;
+    border-left-color: royalblue;
     
-// }
-// 30% {
-//     border-left-color : crimson;
-//     border-top-color: crimson;
-// }
-// 40% {
-//     border-left-color : #22BA1A;
-//     border-top-color: #22BA1A;
-// }
-// 50% {
-//     border-left-color : royalblue;
-//     border-top-color: royalblue;
+}
+30% {
+    border-left-color : crimson;
+    border-top-color: crimson;
+}
+40% {
+    border-left-color : #22BA1A;
+    border-top-color: #22BA1A;
+}
+50% {
+    border-left-color : royalblue;
+    border-top-color: royalblue;
    
-// }
-// 60% {
-//     border-left-color : crimson;
-//     border-top-color: crimson;
+}
+60% {
+    border-left-color : crimson;
+    border-top-color: crimson;
     
-// }
-// 70% {
-//     border-left-color : #22BA1A;
-//     border-top-color: #22BA1A;
-// }
-// 80% {
-//     border-left-color :  royalblue;
-//     border-top-color : royalblue;
-// }
-// 90% {
-//     border-left-color :  crimson;
-//     border-top-color : crimson;
-// }
-// 100% {
-//     border-left-color :  #22BA1A;
-//     border-top-color : #22BA1A; 
-// }
-// `
-// const kfs1 = keyframes
+}
+70% {
+    border-left-color : #22BA1A;
+    border-top-color: #22BA1A;
+}
+80% {
+    border-left-color :  royalblue;
+    border-top-color : royalblue;
+}
+90% {
+    border-left-color :  crimson;
+    border-top-color : crimson;
+}
+100% {
+    border-left-color :  #22BA1A;
+    border-top-color : #22BA1A; 
+}
+`
+const kfs1 = keyframes`
 
-// 100% {
-//     transform: rotateY(360deg);
-// }
-// `
-// const StyledDiv = styled.div`
-// padding: 1rem;
-// border-left : 2px solid gray;
-// border-top : 2px solid gray;
-// border-radius : 5px;
-// display: ${props => props.result ? 'flex' : 'none'};
-// flex-direction : column;
-// align-items : flex-start;
-// margin-left: 5rem;
-// opacity : ${props => props.result ? 1 : 0};
-// backdrop-filter : blur(10px);
-// padding-bottom : 5rem;
-// margin-bottom : 1rem;
-// margin-right : 5rem;
-// color : whitesmoke;
-// animation : ${kf2} 10s ease-in-out infinite; 
-// img {
-//     height: 100px;
-//     width : 100px;
-//     background-color : transparent; 
-//     position : absolute;
-//     right: 5rem;
-//     top: 5rem;
-//     animation: ${kfs1} 10s ease infinite;
-// }
-// div {
-//     padding: .6rem;
-// }
-// h5 {
-//     padding-bottom: 1rem;
-// }
+100% {
+    transform: rotateY(360deg);
+}
+`
+const StyledDiv = styled.div`
+padding: 1rem;
+border-left : 2px solid gray;
+border-top : 2px solid gray;
+border-radius : 5px;
+display: ${props => props.result ? 'flex' : 'none'};
+flex-direction : column;
+align-items : flex-start;
+margin-left: 5rem;
+opacity : ${props => props.result ? 1 : 0};
+backdrop-filter : blur(10px);
+padding-bottom : 5rem;
+margin-bottom : 1rem;
+margin-right : 5rem;
+color : whitesmoke;
+animation : ${kf2} 10s ease-in-out infinite; 
+img {
+    height: 100px;
+    width : 100px;
+    background-color : transparent; 
+    position : absolute;
+    right: 5rem;
+    top: 5rem;
+    animation: ${kfs1} 10s ease infinite;
+}
+div {
+    padding: .6rem;
+}
+h5 {
+    padding-bottom: 1rem;
+}
 
-// `
+`
 
 
 export default function Accordian(props) {
@@ -94,7 +94,7 @@ export default function Accordian(props) {
 
     return (
         <>
-        <div result = {result}>
+        <StyledDiv result = {result}>
             <h5>Birth Year: {birth_year}</h5>
             <h5>Weight : {mass}Kg</h5>
             <h5>Movies:</h5>
@@ -104,7 +104,7 @@ export default function Accordian(props) {
             })   
             }
             <img src ={name === 'Darth Vader' ? sithLogo : rebelLogo}/>
-        </div>
+        </StyledDiv>
         </>
     )
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,91 +1,91 @@
 // Write your Character component here
 import React, {useState, useEffect} from "react";
-// import styled from "styled-components";
-// import { keyframes } from "styled-components";
-// import ReactStrap from 'reactstrap';
+import styled from "styled-components";
+import { keyframes } from "styled-components";
+import ReactStrap from 'reactstrap';
 import Accordian from "./Accordian";
 
-// const kf = keyframes
-// 50% {
-//     opacity: 50%;
-// }
+const kf = keyframes`
+50% {
+    opacity: 50%;
+}
 
-// 100% {
-//     opacity: 100%;
+100% {
+    opacity: 100%;
     
-//     }
-// `
-// const kf1 = keyframes
+    }
+`
+const kf1 = keyframes`
 
-// 100% {
-//     transform: translateX(600px);
-// }
-// `
-// const kf2 = keyframes
-// 20% {
-//     border-color: royalblue;
-// }
-// 30% {
-//     border-color: crimson;
-// }
-// 40% {
-//     border-color: #22BA1A;
-// }
-// 50% {
-//     border-color: royalblue;
+100% {
+    transform: translateX(600px);
+}
+`
+const kf2 = keyframes`
+20% {
+    border-color: royalblue;
+}
+30% {
+    border-color: crimson;
+}
+40% {
+    border-color: #22BA1A;
+}
+50% {
+    border-color: royalblue;
    
-// }
-// 60% {
-//     border-color: crimson;
+}
+60% {
+    border-color: crimson;
     
-// }
-// 70% {
-//     border-color: #22BA1A;
-// }
-// 80% {
-//     border-color : royalblue;
-// }
-// 90% {
-//     border-color : crimson;
-// }
-// 100% {
-//     border-color : #22BA1A; 
-// }
-// `
+}
+70% {
+    border-color: #22BA1A;
+}
+80% {
+    border-color : royalblue;
+}
+90% {
+    border-color : crimson;
+}
+100% {
+    border-color : #22BA1A; 
+}
+`
 
-// const StyledDiv = styled.div`
-// display: flex;
-// align-items: center;
-// border: 2px solid gray;
-// flex-wrap: wrap;
-// margin: 3rem;
-// backdrop-filter: blur(8px);
-// color: black;
-// padding-right: 1rem;
-// font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-// img {
-//     width: 70px;
-//     height: 60px;
-//     background-color: inherit;
-//     transform: rotate(90deg);
-//     margin-left: 1rem;
-//     transform : ${props => props.result ? 'rotate(180deg)' : ''}; 
-//     transition: .3s ease-in-out; 
-// }
-// h3{
-//     animation: ${kf1} .5s ease-in forwards;
-// }
-// div {
-//     border: 1px solid gray;
-//     padding-left: 0;
-//     padding-left: 20rem;
-//    animation : ${kf2} 19s infinite linear; 
-// }
-// opacity: 0;
-// transition: .3s; 
-// animation: ${kf} 1s ease-in-out forwards;
-// `
-
+const StyledDiv = styled.div`
+display: flex;
+align-items: center;
+border: 2px solid gray;
+flex-wrap: wrap;
+margin: 3rem;
+backdrop-filter: blur(8px);
+color: black;
+padding-right: 1rem;
+font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+img {
+    width: 70px;
+    height: 60px;
+    background-color: inherit;
+    transform: rotate(90deg);
+    margin-left: 1rem;
+    transform : ${props => props.result ? 'rotate(180deg)' : ''}; 
+    transition: .3s ease-in-out; 
+}
+h3{
+    animation: ${kf1} .5s ease-in forwards;
+}
+div {
+    border: 1px solid gray;
+    padding-left: 0;
+    padding-left: 20rem;
+   animation : ${kf2} 19s infinite linear; 
+}
+opacity: 0;
+transition: .3s; 
+animation: ${kf} 1s ease-in-out forwards;
+`
+//!I want to put a search bar here but i will need to base it in app
 export default function Character(props) {
     const {name, birthYear, dataOfCharacter,redLightSaber,
         select,setSelect, id,selectedCharacter,setSelectedCharacter} = props;
@@ -100,11 +100,11 @@ export default function Character(props) {
     let result = selectedCharacter === name;
     return (
         <>
-        <div result = {result}>
+        <StyledDiv result = {result}>
             <img onClick = {clickHandler} className="images" src = {redLightSaber}/>
             <h3>{name}</h3>
             <div></div>
-        </div>
+        </StyledDiv>
         <Accordian dataOfCharacter = {dataOfCharacter} selectedCharacter = {selectedCharacter} result = {result}/>
         </>
     )
